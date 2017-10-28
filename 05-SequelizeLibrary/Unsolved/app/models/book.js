3da5336014d9f47d41120fe0fd1bddcaf4cf35ac
Dan <drbrown01@yahoo.com>
2017-10-28 13:20:39 -0500

    updated Project with server and model

--- 05-SequelizeLibrary/Unsolved/app/models/book.js
+++ 05-SequelizeLibrary/Unsolved/app/models/book.js
@@ -15,3 +15,33 @@
 // Sync model with DB
 
 // Export the book model for other files to use
+// Dependencies
+// =============================================================
+
+// This may be confusing but here Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "Chirp" model that matches up with DB
+var Book = sequelize.define("book", {
+  title: {
+    type: Sequelize.STRING
+  },
+  author: {
+    type: Sequelize.STRING
+  },
+  genre: {
+    type: Sequelize.STRING
+  },
+  pages: {
+    type: Sequelize.INTEGER
+  },
+  timestamps: false
+});
+
+// Syncs with DB
+Book.sync();
+
+// Makes the Chirp Model available for other files (will also create a table)
+module.exports = Book;
